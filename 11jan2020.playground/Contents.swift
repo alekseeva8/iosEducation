3783efe7f37a087172ca7c66234b495e540ed954
Education <education@Vsevolods-MacBook-Pro.local>
2020-01-14 21:27:18 +0300

    first commit

--- 11jan2020.playground/Contents.swift
+++ 11jan2020.playground/Contents.swift
@@ -1,7 +1,32 @@
-import UIKit
+let constInteger: Int = 10
+constInteger
+//constInteger = 1 // это константа. так нельзя
+let constDouble: Double = 0
+let constMaxUInt: UInt = UInt.max
+let constMinUInt: UInt = UInt.min
 
-var str = "Hello, playground"
-print (str)
-print ("Hello, world")
+let newConstant = constInteger
+var variable: Int = constInteger
+variable = 55
+var variable2 = variable + 1
+variable2
+variable + 1
 
-// way
+var double: Double = 55
+var doubleDivision = double/12
+var casted = Double(variable)
+
+var boolean1 = true
+boolean1
+var boolean2 = false
+boolean1 == boolean2
+boolean1 != boolean2
+var opposite = !boolean1
+
+var string = "qwe"
+var newLine = "\n"
+var string2 = "hi"
+
+
+
+
